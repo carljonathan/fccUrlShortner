5dc216977b42d65a504cfba8fa9d58c0698fbf39
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-12-05 09:39:02 +0100

    added function to handle get request with urlshort as param

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+// middleware to handle POST requests and read the body tag
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse POST string as JSON
+app.use(bodyParser.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -18,6 +25,17 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl/:urlshort', (req, res) => {
+// take request (original URL) by POST
+  res.json({ "url": req.params.urlshort })
+// check if DB already have the url already
+// if have => return json response with original url and shortened url eg. google, 1
+// else assign number/randomized string to that URL as object
+// save to mongoDB
+// return json response with original url and shortened url eg. google, 1
+// profit
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
