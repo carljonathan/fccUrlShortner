f1d6ca0e7f722f85982ef3c98389423eb4d68e5f
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-12-09 07:22:50 +0100

    refactored the code, updated the database connection, added .use express.json

--- server.js
+++ server.js
@@ -6,24 +6,12 @@ const app = express();
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI)
-const Schema = mongoose.Schema
-
-// create mongoose schema for shortUrl
-const shortUrlSchema = new Schema({
-  // original url
-  original_url: { type: String, required: true },
-  // short url
-  short_url: Number
-})
-
-// create mongoose model for shortUrlSchema
-const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema)
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
-
+app.use(express.json())
 // middleware to handle POST requests and read the body tag
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -31,6 +19,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+const Schema = mongoose.Schema
+
+// create mongoose schema for shortUrl
+const shortUrlSchema = new Schema({
+  // original url
+  original_url: { type: String, required: true },
+  // short url
+  short_url: { type: Number, required: true }
+})
+
+// create mongoose model for shortUrlSchema
+const ShortUrl = mongoose.model('ShortUrl', shortUrlSchema)
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -49,9 +50,12 @@ app.post('/api/shorturl/newUrl', async (req, res) => {
   try {
     const existingUrl = await ShortUrl.findOne({ original_url: url })
     // if the url is not existing in the db, create it
-    if (!existingUrl) {
+    if (existingUrl) {
+      res.json({ original_url: existingUrl.original_url, short_url: existingUrl.short_url })
+    } else {
       let newUrlShort
-      const latestEntry = await ShortUrl.find().sort({ _id: -1 }).limit(1).exec()
+      const latestEntry = await ShortUrl.find().sort({ _id: -1 }).limit(1)
+      console.log(`log latestEntry: ${latestEntry}`)
       if (!latestEntry) {
         newUrlShort = 1
       } else {
@@ -63,37 +67,11 @@ app.post('/api/shorturl/newUrl', async (req, res) => {
       })
       await shortUrl.save()
       res.json(shortUrl)
-    } else {
-      res.json(existingUrl)
     }
-  }
-  // catch error
-  catch (err) {
+  } catch (err) { // catch error
     console.error(err)
+    res.status(500).json('Server Error.')
   }
-  /*
-  if (!result) {
-    async function findLatestAndCreate (done) {
-      let newShort
-      const latestDoc = await ShortUrl.find().sort({ _id: -1 }).limit(1).exec((err, result) => {
-        if (err) console.error(err)
-        done(null, result)
-      })
-      console.log(`latestDoc: ${latestDoc}`)
-      if (!latestDoc) newShort = 1
-      newShort = latestDoc.shortUrl++
-      console.log(`newShort: ${newShort}`)
-      res.json({ latestDoc: latestDoc, newShort: newShort })
-      //const shortUrl = new ShortUrl({ original_url: url, short_url: newShort })
-      shortUrl.save((err, data) => {
-        if (err) console.error(err)
-        done(null, data)
-      //res.json(shortUrl)
-      //})
-    }
-  } else { // if the doc is present
-    res.json(result)
-  }*/
   // if have => return json response with original url and shortened url eg. google, 1
   // else assign number/randomized string to that URL as object
   // save to mongoDB
